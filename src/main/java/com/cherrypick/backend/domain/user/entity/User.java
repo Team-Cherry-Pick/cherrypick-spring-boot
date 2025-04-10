eb992bb65bc39b6cb7e0e92548501b14af5fb3a7
@LYST <101388967+gabury1@users.noreply.github.com>
2025-04-11 01:44:52 +0900

    Feat: RP-26 소셜 로그인 유저 DB 저장 로직 완료

--- src/main/java/com/cherrypick/backend/domain/user/entity/User.java
+++ src/main/java/com/cherrypick/backend/domain/user/entity/User.java
@@ -1,28 +1,48 @@
 package com.cherrypick.backend.domain.user.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
+import java.time.LocalDate;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Optional;
 
 @Entity
-@Getter
-@Setter
+@Getter @AllArgsConstructor @Builder
+@Setter @RequiredArgsConstructor @ToString
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
 
-    private Long providerId;
-    private String name;
-    private String email;
-    private Date birthday;
-    private String gender;
-    private String provider;
-    private boolean status;
+    private String oauthId;           // 소셜로그인 업체에서 받은 ID
+    private String nickname;          // 유저 닉네임
+    private String email;             // 이메일
+    private LocalDate birthday;            // 생일
+    private String gender;            // 성별
+    private String provider;          // 업체명 ex) kakao
+    @Enumerated(EnumType.STRING)
+    private UserStatus status;        // 소프트 딜리트 (삭제 상태면
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public static User fromKakao(OAuth2User oauth2User) {
+
+        var userAttr = oauth2User.getAttributes();
+
+        return User.builder()
+                .oauthId(userAttr.get("id").toString())
+                .nickname(Optional.ofNullable((HashMap<String, String>)userAttr.get("properties")).map(p -> p.get("nickname")).get().toString())
+                .email("example@example.com")
+                .birthday(LocalDate.of(1999, 12, 31))
+                .provider("kakao")
+                .gender("male")
+                .status(UserStatus.ACTIVE)
+                .role(Role.CLIENT)
+                .build();
+    }
+
 }
---
+++ src/main/java/com/cherrypick/backend/domain/user/oauth/UserLoginDTO.java
@@ -0,0 +1,46 @@
+package com.cherrypick.backend.domain.user.oauth;
+
+import com.cherrypick.backend.domain.user.entity.Role;
+import com.cherrypick.backend.domain.user.entity.User;
+import lombok.Builder;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+@Builder
+public record UserLoginDTO(
+        Long userId,
+        String oauthId,
+        String nickname,
+        Role role
+) implements OAuth2User
+{
+
+    // 엔티티로부터 DTO를 만든다.
+    public static UserLoginDTO from(User user) {
+        return UserLoginDTO.builder()
+                .userId(user.getUserId())
+                .oauthId(user.getOauthId())
+                .nickname(user.getNickname())
+                .role(user.getRole())
+                .build();
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return Map.of();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(role);
+    }
+
+    @Override
+    public String getName() {
+        return oauthId;
+    }
+}
