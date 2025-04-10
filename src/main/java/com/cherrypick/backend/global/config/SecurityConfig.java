03cce190697e80db9a7268734070a1df63d39ae6
@LYST <101388967+gabury1@users.noreply.github.com>
2025-04-10 22:59:00 +0900

    Feat: RP-26 카카오 소셜 로그인 연동

---
+++ src/main/java/com/cherrypick/backend/global/config/SecurityConfig.java
@@ -0,0 +1,76 @@
+package com.cherrypick.backend.global.config;
+
+import com.cherrypick.backend.domain.user.service.OAuth2Service;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity @RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final OAuth2Service oauth2Service;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+    
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        //csrf disable
+        http
+                .csrf((auth) -> auth.disable());
+        //HTTP Basic 인증 방식 disable
+        http
+                .httpBasic((auth) -> auth.disable());
+
+        http
+                .formLogin(AbstractHttpConfigurer::disable);
+        
+        // oauth2
+	    http
+                .oauth2Login((oauth2) -> oauth2
+                                .loginPage("/login")
+                                .userInfoEndpoint((userInfoEndpointConfig) -> userInfoEndpointConfig
+                                .userService(oauth2Service))
+                                .defaultSuccessUrl("/")
+                        );
+                                
+                
+        // 로그아웃 경로
+        http
+                .logout( logout -> logout
+                                .logoutUrl("/logout")
+                                .logoutSuccessUrl("/")
+
+                );
+
+        //경로별 인가 작업
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/").permitAll()
+                        .anyRequest().permitAll());
+
+
+        return http.build();
+    }
+
+
+}
