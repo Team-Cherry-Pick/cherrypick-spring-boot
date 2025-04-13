fe2b1980cd0700489b7ae28e1bef58313f718d44
홍선아 <144754093+HongSeonAh@users.noreply.github.com>
2025-04-13 16:21:34 +0900

    Feat: 핫딜 게시글 CRUD DTO 생성

---
+++ src/main/java/com/cherrypick/backend/domain/deal/dto/response/DealDetailResponseDTO.java
@@ -0,0 +1,31 @@
+package com.cherrypick.backend.domain.deal.dto.response;
+
+import com.cherrypick.backend.domain.deal.vo.Price;
+import com.cherrypick.backend.domain.deal.vo.Shipping;
+import com.cherrypick.backend.domain.image.vo.ImageUrl;
+import com.cherrypick.backend.domain.user.vo.User;
+import com.cherrypick.backend.domain.store.vo.Store;
+
+import java.util.List;
+
+public record DealDetailResponseDTO (
+
+        Long dealId,
+        List<ImageUrl> imageUrls,
+        User user,
+        Store store,
+        List<String> categorys,
+        String title,
+        List<String> infoTags,
+        Shipping shipping,
+        Price price,
+        String content,
+        int totalViews,
+        int totalLikes,
+        int totalUnLikes,
+        int totalComments,
+        String deepLink,
+        String originalUrl,
+        boolean isSoldOut
+) {
+}
