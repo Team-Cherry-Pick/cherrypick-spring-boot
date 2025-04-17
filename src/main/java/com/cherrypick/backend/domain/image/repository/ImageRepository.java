f56d7daf6f41de9b827de2721124bb9cdeedcf6c
홍선아 <144754093+HongSeonAh@users.noreply.github.com>
2025-04-17 15:09:22 +0900

    Feat: RP-20 임시 이미지 삭제 로직

--- src/main/java/com/cherrypick/backend/domain/image/repository/ImageRepository.java
+++ src/main/java/com/cherrypick/backend/domain/image/repository/ImageRepository.java
@@ -3,5 +3,10 @@ package com.cherrypick.backend.domain.image.repository;
 import com.cherrypick.backend.domain.image.entity.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ImageRepository extends JpaRepository<Image, Long> {
+
+    // isTemp = true인 이미지 전체 조회
+    List<Image> findAllByIsTempTrue();
 }
