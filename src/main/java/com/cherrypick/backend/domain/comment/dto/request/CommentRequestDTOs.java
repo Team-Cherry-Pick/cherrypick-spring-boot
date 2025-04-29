322dd24e13cacb58bf527842164bb8c5bffbd7fc
홍선아 <144754093+HongSeonAh@users.noreply.github.com>
2025-04-29 15:25:49 +0900

    feat: RP-14 댓글 DTO 생성

---
+++ src/main/java/com/cherrypick/backend/domain/comment/dto/request/CommentRequestDTOs.java
@@ -0,0 +1,7 @@
+package com.cherrypick.backend.domain.comment.dto.request;
+
+public class CommentRequestDTOs {
+
+    public record Create(Long parentId, String content){}
+    public record Like(Long commentID, boolean isLike){}
+}
---
+++ src/main/java/com/cherrypick/backend/domain/comment/dto/response/CommentListResponseDTO.java
@@ -0,0 +1,18 @@
+package com.cherrypick.backend.domain.comment.dto.response;
+
+import com.cherrypick.backend.domain.user.vo.User;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record CommentListResponseDTO(
+        Long commentId,
+        Long parentId,
+        User user,
+        String content,
+        int totalLikes,
+        int totalReplys,
+        LocalDateTime createdAt,
+        boolean isDelete,
+        List<CommentListResponseDTO> replies
+) { }
