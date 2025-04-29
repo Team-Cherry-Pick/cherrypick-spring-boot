418bd7148a63473e8450f77b4677b0b3a1d3af36
홍선아 <144754093+HongSeonAh@users.noreply.github.com>
2025-04-29 12:33:24 +0900

    feat: RP-10 Swagger 구현

--- src/main/java/com/cherrypick/backend/domain/image/controller/ImageController.java
+++ src/main/java/com/cherrypick/backend/domain/image/controller/ImageController.java
@@ -4,6 +4,8 @@ import com.cherrypick.backend.domain.image.dto.request.ImageUploadRequestDTO;
 import com.cherrypick.backend.domain.image.dto.response.ImageDeleteResponseDTO;
 import com.cherrypick.backend.domain.image.dto.response.ImageUploadResponseDTO;
 import com.cherrypick.backend.domain.image.service.ImageService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@Tag(name = "Image", description = "이미지 생성 및 삭제")
 @RequestMapping("/api/image")
 @RequiredArgsConstructor
 public class ImageController {
@@ -18,6 +21,10 @@ public class ImageController {
     private final ImageService imageService;
 
     // 이미지 업로드
+    @Operation(
+            summary = "이미지 업로드 API V1",
+            description = "이미지를 업로드하면 S3 URL을 반환합니다. 인덱스와 이미지 개수가 매칭돼야 하며 인덱스는 0부터 시작합니다."
+    )
     @PostMapping
     public ResponseEntity<List<ImageUploadResponseDTO>> uploadImages(
             @ModelAttribute ImageUploadRequestDTO dto,
@@ -28,6 +35,10 @@ public class ImageController {
     }
 
     // 이미지 삭제
+    @Operation(
+            summary = "이미지 삭제 API V1",
+            description = "이미지를 RDS 및 S3에서 삭제합니다. 매일 새벽 3시 자동 실행되어 임시 이미지들이 삭제됩니다."
+    )
     @DeleteMapping("/{imageId}")
     public ResponseEntity<ImageDeleteResponseDTO> deleteImage(
             @PathVariable Long imageId,
