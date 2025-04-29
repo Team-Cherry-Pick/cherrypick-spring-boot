09611aca1d1aa5d803fb5f9a3d2507b471a90d2f
홍선아 <144754093+HongSeonAh@users.noreply.github.com>
2025-04-29 16:35:22 +0900

    feat: RP-14 댓글 좋아요 API 구현

---
+++ src/main/java/com/cherrypick/backend/domain/comment/repository/CommentLikeRepository.java
@@ -0,0 +1,8 @@
+package com.cherrypick.backend.domain.comment.repository;
+
+import com.cherrypick.backend.domain.comment.entity.CommentLike;
+import com.cherrypick.backend.domain.comment.entity.CommentLikeId;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentLikeRepository extends JpaRepository<CommentLike, CommentLikeId> {
+}
