be341129dca4ab3588ecd3a26800b25858187963
홍선아 <144754093+HongSeonAh@users.noreply.github.com>
2025-04-29 12:33:24 +0900

    feat: RP-10 Swagger 구현

--- src/main/java/com/cherrypick/backend/domain/deal/controller/DealController.java
+++ src/main/java/com/cherrypick/backend/domain/deal/controller/DealController.java
@@ -7,6 +7,8 @@ import com.cherrypick.backend.domain.deal.dto.response.DealDetailResponseDTO;
 import com.cherrypick.backend.domain.deal.dto.response.DealResponseDTOs;
 import com.cherrypick.backend.domain.deal.dto.response.DealSearchResponseDTO;
 import com.cherrypick.backend.domain.deal.service.DealService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RequiredArgsConstructor
+@Tag(name = "Deal", description = "핫딜 게시글 CRUD")
 @RestController
 @RequestMapping("/api")
 public class DealController {
@@ -21,6 +24,10 @@ public class DealController {
     private final DealService dealService;
 
     // 게시글 생성
+    @Operation(
+            summary = "핫딜 게시글 생성 API V1",
+            description = "핫딜 게시글을 생성합니다. JWT 인증 필수입니다."
+    )
     @PostMapping("/deal")
     public ResponseEntity<DealResponseDTOs.Create> createDeal(
             @RequestParam(value = "version", defaultValue = "v1") String version,
@@ -31,6 +38,10 @@ public class DealController {
     }
 
     // 게시글 전체 조회 (검색)
+    @Operation(
+            summary = "핫딜 게시글 전제 조회 API V1",
+            description = "핫딜 게시글을 전체 조회합니다. 필터를 보낼 경우 부분 조회(검색) 기능을 합니다."
+    )
     @PostMapping("/search/deal")
     public ResponseEntity<List<DealSearchResponseDTO>> searchDeals(
             @RequestParam(value = "version", defaultValue = "v1") String version,
@@ -45,6 +56,10 @@ public class DealController {
     }
 
     // 게시글 상세 조회
+    @Operation(
+            summary = "핫딜 게시글 상세 조회 API V1",
+            description = "핫딜 게시글을 상세 조회합니다."
+    )
     @GetMapping("/deal/{dealId}")
     public ResponseEntity<DealDetailResponseDTO> getDealDetail(
             @PathVariable Long dealId,
@@ -54,6 +69,10 @@ public class DealController {
     }
 
     // 게시물 수정
+    @Operation(
+            summary = "핫딜 게시글 수정 API V1",
+            description = "핫딜 게시글을 수정합니다. 수정 할 부분만 요청하면 됩니다. JWT 인증 필수입니다."
+    )
     @PatchMapping("/deal")
     public ResponseEntity<DealResponseDTOs.Update> updateDeal(
             @RequestBody DealUpdateRequestDTO dto,
@@ -63,6 +82,10 @@ public class DealController {
     }
 
     // 게시글 삭제
+    @Operation(
+            summary = "핫딜 게시글 삭제 API V1",
+            description = "핫딜 게시글을 삭제합니다. SoftDelete 방식이며 JWT 인증 필수입니다."
+    )
     @DeleteMapping("/deal/{dealId}")
     public ResponseEntity<DealResponseDTOs.Delete> deleteDeal(
             @PathVariable Long dealId,
