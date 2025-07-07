f849586995add7868778c57e6c186cdd6ccb10f8
@LYST <gabury1@gmail.com>
2025-07-08 04:13:03 +0900

    Feat: RP-102 스토어 캐싱 구현

--- src/main/java/com/cherrypick/backend/domain/store/service/StoreService.java
+++ src/main/java/com/cherrypick/backend/domain/store/service/StoreService.java
@@ -4,21 +4,31 @@ import com.cherrypick.backend.domain.store.dto.StoreResponseListDTO;
 import com.cherrypick.backend.domain.store.repository.StoreRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
 
 @Service @Slf4j @RequiredArgsConstructor
 public class StoreService {
 
     private final StoreRepository storeRepository;
+    private final RedisTemplate<String, Object> redisTemplate;
 
     public StoreResponseListDTO getStoreList()
     {
         var list = storeRepository.findAll();
-        return new StoreResponseListDTO(
-                list.stream()
-                .map(StoreResponseListDTO.StoreResponseDTO::from)
-                .toList()
-        );
+        var response = redisTemplate.opsForValue().get("cache:stores");
+        if(response == null)
+        {
+            response = new StoreResponseListDTO(
+                    list.stream()
+                            .map(StoreResponseListDTO.StoreResponseDTO::from)
+                            .toList());
+            redisTemplate.opsForValue().set("cache:stores", response, 1, TimeUnit.DAYS);
+        }
+
+        return (StoreResponseListDTO) response;
     }
 
 }
