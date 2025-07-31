f5c6bd7aba36dfacf184f6ee99e1b8bb27832b3b
@LYST <gabury1@gmail.com>
2025-07-31 23:03:29 +0900

    Refactor: RP-113 요청 함수화

--- src/main/java/com/cherrypick/backend/domain/deal/adapter/out/OpenAiAdapter.java
+++ src/main/java/com/cherrypick/backend/domain/deal/adapter/out/OpenAiAdapter.java
@@ -28,7 +28,6 @@ public class OpenAiAdapter {
     private final LogService logService;
 
     public Optional<String> requestClassify(String prompt) {
-        RestTemplate restTemplate = new RestTemplate();
 
         Map<String, Object> requestMap = new HashMap<>();
         requestMap.put("model", "gpt-3.5-turbo");
@@ -36,51 +35,10 @@ public class OpenAiAdapter {
         requestMap.put("messages", List.of(Map.of("role", "user", "content", prompt)));
         requestMap.put("temperature", 0.0);
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        String requestBody = "";
-        try {
-            requestBody = objectMapper.writeValueAsString(requestMap);
-        } catch (JsonProcessingException e) {
-            System.err.println("요청 본문 직렬화 오류: " + e.getMessage());
-        }
-
-        org.springframework.http.HttpHeaders headers = new org.springframework.http.HttpHeaders();
-        headers.set("Content-Type", "application/json");
-        headers.set("Authorization", "Bearer " + openAiApiKey);
-
-        org.springframework.http.HttpEntity<String> request = new org.springframework.http.HttpEntity<>(requestBody, headers);
-
-        try {
-
-            org.springframework.http.ResponseEntity<String> responseEntity =
-                    restTemplate.exchange(openAiApiUrl, org.springframework.http.HttpMethod.POST, request, String.class);
-
-            if (responseEntity.getStatusCode().is2xxSuccessful()) {
-                String response = responseEntity.getBody();
-                JsonNode rootNode = objectMapper.readTree(response);
-                var rawText = rootNode.path("choices").get(0).path("message").path("content").asText();
-
-                JsonNode usage = rootNode.path("usage");
-                int promptTokens = usage.path("prompt_tokens").asInt();
-                int completionTokens = usage.path("completion_tokens").asInt();
-                int totalTokens = usage.path("total_tokens").asInt();
-
-                logService.openAiLog(promptTokens, completionTokens, totalTokens);
-
-                return Optional.ofNullable(rawText);
-
-            } else {
-                System.err.println("OpenAI 요청 실패: " + responseEntity.getStatusCode());
-                return null;
-            }
-        } catch (IOException e) {
-            System.err.println("OpenAI 호출 오류: " + e.getMessage());
-            return null;
-        }
+        return Optional.ofNullable(request(requestMap));
     }
 
     public Optional<String> requestContent(String prompt) {
-        RestTemplate restTemplate = new RestTemplate();
 
         Map<String, Object> requestMap = new HashMap<>();
         requestMap.put("model", "gpt-4o");
@@ -88,6 +46,13 @@ public class OpenAiAdapter {
         requestMap.put("messages", List.of(Map.of("role", "user", "content", prompt)));
         requestMap.put("temperature", 1.0);
 
+        return Optional.ofNullable(request(requestMap));
+    }
+
+
+    private String request(Map<String, Object> requestMap) {
+
+        RestTemplate restTemplate = new RestTemplate();
         ObjectMapper objectMapper = new ObjectMapper();
         String requestBody = "";
         try {
@@ -119,7 +84,7 @@ public class OpenAiAdapter {
 
                 logService.openAiLog(promptTokens, completionTokens, totalTokens);
 
-                return Optional.ofNullable(rawText);
+                return rawText;
 
             } else {
                 System.err.println("OpenAI 요청 실패: " + responseEntity.getStatusCode());
@@ -129,7 +94,8 @@ public class OpenAiAdapter {
             System.err.println("OpenAI 호출 오류: " + e.getMessage());
             return null;
         }
-
     }
+
+
 
 }
