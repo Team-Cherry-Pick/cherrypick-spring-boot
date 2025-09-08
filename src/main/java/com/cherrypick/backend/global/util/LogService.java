952d1da1eb80b0cb119089902da8eada6f3cd221
@LYST <gabury1@gmail.com>
2025-09-08 15:40:02 +0900

    Feat: RP-148 구매버튼 클릭 시 로그가 찍힘

--- src/main/java/com/cherrypick/backend/global/util/LogService.java
+++ src/main/java/com/cherrypick/backend/global/util/LogService.java
@@ -143,6 +143,28 @@ public class LogService {
         MDC.remove("logType");
     }
 
+    // 구매버튼 클릭 로그
+    public void clickPurchaseLog(Long userId,
+                                 String deviceId,
+                                 Long dealId,
+                                 Long categoryId,
+                                 String categoryName,
+                                 String dealTitle) {
+        mdcInitialize();
+        MDC.put("logType", "PURCHASE_CLICK_LOG");
+
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("purchaseclick_userId", Optional.ofNullable(userId).orElse(-1L));
+        map.put("purchaseclick_deviceId", Optional.ofNullable(deviceId).orElse("unknown"));
+        map.put("purchaseclick_dealId", Optional.ofNullable(dealId).orElse(-1L));
+        map.put("purchaseclick_categoryId", Optional.ofNullable(categoryId).orElse(-1L));
+        map.put("purchaseclick_categoryName", Optional.ofNullable(categoryName).orElse("unknown"));
+        map.put("purchaseclick_dealTitle", Optional.ofNullable(dealTitle).orElse("unknown"));
+        log.info(toJson(map));
+
+        MDC.remove("logType");
+    }
+
     public void openAiLog(Integer promptTokens, Integer completionTokens, Integer totalTokens)
     {
         mdcInitialize();
