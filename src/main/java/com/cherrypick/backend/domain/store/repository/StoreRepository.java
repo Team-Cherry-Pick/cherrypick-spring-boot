a1e389b884cbebc483f782c82040c969203e2894
@LYST <gabury1@gmail.com>
2025-09-30 14:58:04 +0900

    Feat: RP-183 보이지 않는 스토어 추가 (FMkorea 등 핫딜 커뮤니티들)

--- src/main/java/com/cherrypick/backend/domain/store/repository/StoreRepository.java
+++ src/main/java/com/cherrypick/backend/domain/store/repository/StoreRepository.java
@@ -2,6 +2,15 @@ package com.cherrypick.backend.domain.store.repository;
 
 import com.cherrypick.backend.domain.store.entity.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface StoreRepository extends JpaRepository<Store, Long> {
+
+    @Query(value="SELECT * FROM store WHERE is_blind=false", nativeQuery = true)
+    List<Store> findAllVisible();
+
 }
